695eff50015413a5ab753002161444d9418dd1e1
Victor Javier <VictorJavierGuamnChulunchana@gmail.com>
2019-11-17 14:05:26 +0000

    removing sths

--- api/movies/index.js
+++ api/movies/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 let controller = require('./controller');
 
-router.get('/', (req, res) => res.json(controller.getMovies()));
+
 router.get('/:id', (req, res) => res.json(controller.getSingleMovie(req.params.id)));
 router.post('/create', (req, res) => res.json(controller.createMovie(req.body)));
 router.post('/delete/:id', (req, res) => res.json(controller.deleteMovie(req.params.id)));
