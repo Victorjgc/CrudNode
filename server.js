2713417d3dfe47c32735abbc632e9d83165290f0
Victor Javier <VictorJavierGuamnChulunchana@gmail.com>
2019-11-19 16:07:12 +0000

    sth

--- server.js
+++ server.js
@@ -3,24 +3,23 @@ const morgan = require('morgan');
 const compression = require('compression');
 const methodOverride = require('method-override');
 const notifier = require('node-notifier');
-const session = require('express-session'); 
+const session = require('express-session');
 const rateLimit = require("express-rate-limit");
 const app = express();
 const moviesRouter = require('./api/movies');
 const cors = require('cors');
 
-const corsOptions = {
+const corsOptions = {//¿?
     origin: ['http://localhost:3000/movies']
 };
 
 const apiLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 5, 
+    max: 5,
     message:
-    "Too many requests,  please try again after an hour"
+        "Too many requests,  please try again after an 15 min"
 });
 app.use(apiLimiter);
-
 app.use(express.json());//añadir estas líneas siempre
 app.use(session({ secret: '1234' }));
 app.use(morgan('combined'));//MORGAN nos da todos los logs por la terminal
@@ -28,21 +27,21 @@ app.use(compression());
 app.use('/movies', moviesRouter);
 app.use(cors(corsOptions));
 function errorHandler(err, req, res, next) {
-    if (!err) { 
-        return next(); 
-    } 
+    if (!err) {
+        return next();
+    }
     if (err) {
-    const message = `Error en ${req.method} ${req.url}`;
-    notifier.notify({ title: 'Error', message });
-    res.status(500).send('Algo se ha roto');
-    }   
-  }
+        const message = `Error en ${req.method} ${req.url}`;
+        notifier.notify({ title: 'Error', message });
+        res.status(500).send('Algo se ha roto');
+    }
+}
 
 if (process.env.NODE_ENV === 'development') {
     app.use(methodOverride());
     app.use(errorHandler);
 }
- 
+
 
 
 app.listen(3000, () => console.log('Ready on port 3000!'));
