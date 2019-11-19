f2df515d04e8afe9dc29ba9d8d22c665f422dd06
Victor Javier <VictorJavierGuamnChulunchana@gmail.com>
2019-11-19 16:35:03 +0000

    sth

--- api/movies/__tests__/controller.test.js
+++ api/movies/__tests__/controller.test.js
@@ -1,6 +1,18 @@
-const singleMovie = require('../controller').getSingleMovie;
+/*
+
+const controller = require('../controller');
+
+
+
 describe('controller', () => {
     describe('singleMovie', () => {
 
     })
-});
+});//npi
+
+
+describe('Movies_controller',() => {
+    test('Debe devolver el json con las peliculas',() =>{
+        expect(controller.getMovies()).toBe();
+    })
+});*/
