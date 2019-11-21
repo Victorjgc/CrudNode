61a2c7072c3751a4b63a6445658637334053c13c
Victor Javier <VictorJavierGuamnChulunchana@gmail.com>
2019-11-21 13:36:59 +0000

    sth

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const rateLimit = require("express-rate-limit");
 const app = express();
 const moviesRouter = require('./api/movies');
 const cors = require('cors');
-
+const request = require('superagent');
 const corsOptions = {
     origin: ['http://localhost:3000/movies']
 };
@@ -19,14 +19,33 @@ const apiLimiter = rateLimit({
     message:
     "Too many requests,  please try again after an hour"
 });
-app.use(apiLimiter);
 
+//para login
+function decodeBase64(str) {//mi no entender
+    return Buffer.from(str, 'base64').toString()
+}
+
+function authentication(req, res, next) {
+    if (req.method === 'GET') {
+      return next();
+    }
+    const basicAuth = req.headers.authorization.split(' ')[1];
+    const userPassword = decodeBase64(basicAuth).split(':');
+    if (userPassword[0] === 'Victor' && userPassword[1] === 'Javier') {
+      return next();
+    }
+    res.status(500).send('Permiso denegado');
+}
+
+app.use(request);
+app.use(apiLimiter);
 app.use(express.json());//añadir estas líneas siempre
 app.use(session({ secret: '1234' }));
 app.use(morgan('combined'));//MORGAN nos da todos los logs por la terminal
 app.use(compression());
 app.use('/movies', moviesRouter);
 app.use(cors(corsOptions));
+app.use(authentication);
 function errorHandler(err, req, res, next) {
     if (!err) { 
         return next(); 
@@ -42,7 +61,6 @@ if (process.env.NODE_ENV === 'development') {
     app.use(methodOverride());
     app.use(errorHandler);
 }
- 
 
 
 app.listen(3000, () => console.log('Ready on port 3000!'));
